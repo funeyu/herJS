12c006e2de8fdd8237276ed9e6ee0175f8fe1f63
funeyu <funeyu@gmail.com>
2018-01-30 22:55:38 +0800

    添加test

--- h.js
+++ h.js
@@ -14,7 +14,46 @@ const getLineInfo = exports.getLineInfo = function(input, offset) {
     }
 }
 
-console.log(
-    getLineInfo("java" + "\n" +
-    "elcoooooo", 8)
-)
+const _break = {keyword: 'break'},
+      _case = {keyword: 'case'},
+      _catch = {keyword: 'catch'},
+      _continue = {keyword: 'continue'},
+      _default = {keyword: 'default'},
+      _do = {keyword: 'do'},
+      _else = {keyword: 'else'},
+      _for = {keyword: 'for'},
+      _function = {keyword: 'function'},
+      _if = {keyword: 'if'},
+      _return = {keyword: 'return'},
+      _switch = {keyword: 'switch'},
+      _throw = {keyword: 'throw'},
+      _try = {keyword: 'try'},
+      _var = {keyword: 'var'},
+      _while = {keyword: 'while'},
+      _new = {keyword: 'new'},
+      _false = {keyword: 'false', value: false},
+      _true = {keyword: 'true', value: true},
+      _null = {keyword: 'null', value: null};
+
+const KeyWords = {
+    'break': _break,
+    'case': _case,
+    'catch': _catch,
+    'continue': _continue,
+    'default': _default,
+    'do': _do,
+    'else': _else,
+    'for': _for,
+    'function': _function,
+    'if': _if,
+    'return': _return,
+    'switch': _switch,
+    'throw': _throw,
+    'try': _try,
+    'var': _var,
+    'while': _while,
+    'new': _new,
+    'false': _false,
+    'true': _true,
+    'null': _null
+}
---
+++ test.js
@@ -0,0 +1,6 @@
+const getLineInfo = require('./h').getLineInfo
+
+console.log(
+    getLineInfo("java" + "\n" +
+        "elcoooooo", 8)
+)
